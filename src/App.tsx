de85b7ce24e2ebf9e5943e44300d2377c89a600a
LevPewPew <levthedev@protonmail.com>
2022-11-24 16:34:59 +1100

    basic setup apollo client and graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import { useState } from 'react'
+import { useQuery, gql } from '@apollo/client';
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
 function App() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
   const [count, setCount] = useState(0)
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
 
   return (
     <div className="App">
@@ -30,6 +46,20 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <br />
+      <div>
+        {data.locations.map(({ id, name, description, photo }: any) => (
+          <div key={id}>
+            <h3>{name}</h3>
+            <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+            <br />
+            <b>About this location:</b>
+            <p>{description}</p>
+            <br />
+          </div>
+        ))
+      }
+      </div>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 import App from './App'
 import './index.css'
 
+const client = new ApolloClient({
+  uri: 'https://flyby-gateway.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
-)
+);
